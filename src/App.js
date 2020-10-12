4431fa7bc069117460a8b9dfed42fcf40417fcc7
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-10-12 22:07:53 +0530

    Basic UI Setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div>
 
+    const [token, setToken] = useState('')
 
-    </div>
-  );
+    return (
+        <div>
+            <header>
+                <nav>JWT Debugger</nav>
+            </header>
+            <div className="container">
+                <div className="input">
+                    <h1>JWT Token</h1>
+                    <form action="">
+                        <input type="text" value={token} onChange={e=> setToken(e.target.value)}/>
+                    </form>
+                </div>
+                <div className="output">
+                    <h2>Header</h2>
+                    <div></div>
+                    <h2>Payload / Claims</h2>
+                    <div></div>
+                    <h2>Signature</h2>
+                    <div></div>
+                </div>
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
