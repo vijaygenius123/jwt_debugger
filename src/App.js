7daaafd3ed7438accdab1d87a8c9d739505b820c
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-10-13 09:06:51 +0530

    Added state for header, payload and signature

--- src/App.js
+++ src/App.js
@@ -3,7 +3,33 @@ import './App.css';
 
 function App() {
 
-    const [token, setToken] = useState('')
+    const [token, setToken] = useState('');
+    const [error, setError] = useState(false)
+
+    const [header, setHeader] = useState('');
+    const [payload, setPayload] = useState('');
+    const [signature, setSignature] = useState('');
+
+
+    const handleToken = (e) => {
+        if (e.target.value) {
+
+            const tokens = e.target.value.split('.');
+            if (tokens.length === 3) {
+                setError(false);
+
+            } else {
+                setError(true);
+            }
+
+            console.log(tokens)
+
+        }
+        console.log(e.target.value);
+
+
+        setToken(e.target.value);
+    }
 
     return (
         <div>
@@ -11,9 +37,9 @@ function App() {
                 <nav>JWT Debugger</nav>
             </header>
             <div className="container">
-                <div className="input">
+                <div className={`input ${error ? 'error' : ''}`}>
                     <h1>JWT Token</h1>
-                        <textarea type="text" value={token} onChange={e=> setToken(e.target.value)}/>
+                    <textarea type="text" value={token} onChange={handleToken}/>
                 </div>
                 <div className="output">
                     <h2>Header</h2>
